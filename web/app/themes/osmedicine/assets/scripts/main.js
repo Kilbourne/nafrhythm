c2e24d4767ac4d8d3766844784d8b8eb2dfa22d4
Kilbourne <luca.castellone@gmail.com>
2016-03-24 12:35:01 +0100

    JS

--- web/app/themes/osmedicine/assets/scripts/main.js
+++ web/app/themes/osmedicine/assets/scripts/main.js
@@ -10,7 +10,8 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  * ======================================================================== */
 
-(function($) {
+;(function($) {
+  var disc_cache=[];
 
   // Use this variable to set up the common and page specific functions. If you
   // rename this variable, you will also need to rename the namespace below.
@@ -18,10 +19,36 @@
     // All pages
     'common': {
       init: function() {
-        // JavaScript to be fired on all pages
-         $("#Glide").glide({
-          autoplay:false
-        });
+        (function Menu(){
+          var isOpen=false;
+          $(document).on('click',function(e){ if (e.which != 2 && !$(e.target).closest('#responsive-menu, #click-menu').length) { closeRM() }});
+          $(document).on('click', '#click-menu', function() { !isOpen ? openRM() : closeRM() });
+          function openRM() {
+              //$('#responsive-menu').css('display', 'block');
+              $('#responsive-menu').addClass('RMOpened');
+              $('#click-menu').addClass('click-menu-active');
+              $('#responsive-menu').stop().animate({ left: "0" }, 500, 'linear', function() {
+                  //$('#responsive-menu').css('height', $(document).height());
+                  isOpen = true
+              })
+          }
+
+          function closeRM() {
+              $('#responsive-menu').animate({ left: -$('#responsive-menu').width() }, 500, 'linear', function() {
+                  //$('#responsive-menu').css('display', 'none');
+                  $('#responsive-menu').removeClass('RMOpened');
+                  $('#click-menu').removeClass('click-menu-active');
+                  isOpen = false
+              })
+          }
+          $(window).resize(function() {
+              $('#responsive-menu').stop(true, true);
+              //$('#responsive-menu').css('height', $(document).height());
+              if ($(window).width() > 800) {
+                  if ($('#responsive-menu').css('left') != -$('#responsive-menu').width()) { closeRM() }
+              }
+          });
+        }())
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
@@ -30,31 +57,128 @@
     // Home page
     'home': {
       init: function() {
-        // JavaScript to be fired on the home page
+        $('.full-slider').unslider({
+          arrows:false,
+          animation:"fade",
+          autoplay:true
+        });
+        $('.full-slider').unslider('initSwipe');
       },
       finalize: function() {
         // JavaScript to be fired on the home page, after the init JS
       }
     },
-    'singlepost':{
-      init: function(){        
-        var widthArr=[];
-        $('.related-wrap').each(function(i){
-          var height= $(this).height()+1;
-          widthArr.push(height);
-          if(i===$('.related-wrap').length-1){
-            var final=Math.max.apply(null,widthArr);
-            $('.related-post-img-wrap img').height(final);
-          }
-        });
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
+    'discografia':{
       init: function() {
-        // JavaScript to be fired on the about us page
+        var music = document.getElementById('music');
+          $('.disco-link').click(ajaxDisco);
+          $('#pButton').click(playAudio);
+          $('#music').on('ended',resetPlayAudio) 
+
+          History.Adapter.bind(window,'statechange',function statechangeCallback (){ // Note: We are using statechange instead of popstate
+           var State = History.getState(); // Note: We are using History.getState() instead of event.state
+           var data=State.data.data,
+              
+           p=$('#'+State.data.target);
+
+                                       $('.extended-disc-panel').fadeOut('400', function() {
+                                        stopAudio();
+                              $('.discs-list>li.active').add(p).toggleClass('active');
+                            $(".disco-title").text(data.title);
+                            var src= $(".copertina>img");
+                            src.remove();                            
+                            $(".copertina").append(data.thumb);
+                            //$(target).children('img').remove()
+                            //$(target).append(src);
+                            $(".tracklist>div").html(data.tracklist);
+                            $(".descrizione-disco>div").html(data.excerpt);
+                            if(data.audio_sample){
+                              music.src=data.audio_sample;
+                              $('#pButton').show();
+                            }else{
+                              $('#pButton').hide();
+                            }  
+                            $('.extended-disc-panel').fadeIn('400',function(){
+                                     $('.extended-disc-panel').removeClass('not-visible');
+                            });
+                            });
+
+           //if(State.data.disco){
+            //$('.disco-link[href="'+State.data.disco+'"').click();
+           //}
+          } );
+                    if(document.location.toString().indexOf('?disco=')>-1){
+            var searched=document.location.toString().split('?disco=')[1];
+            $('.disco-link').filter(function(){
+              var splitted =this.href.split('/'),
+                            location=splitted[splitted.length-1]!==''?splitted[splitted.length-1]:splitted[splitted.length-2];
+                            return location === searched;
+                          }).click();
+            History.Adapter.trigger(window,'statechange')
+          }else{         
+            $('.extended-disc-panel').removeClass('not-visible');
+          }
+          function ajaxDisco(e){
+            e.preventDefault();
+            var cached;
+            var target=e.currentTarget,url=target.href;
+            cached=JSON.parse(localStorage.getItem(url));
+var splitted=url.split('/'),
+                            location=splitted[splitted.length-1]!==''?splitted[splitted.length-1]:splitted[splitted.length-2];
+            if(cached !==null){
+              History.pushState(cached, cached.data.title, '?disco='+location);
+            }else{
+            $.post( gesualdi.ajaxurl, {
+                            action: 'gesualdi_disco',
+                            postlink: url ,
+                            nonce:gesualdi.nonce
+                        },
+
+                        function(response) {
+                          if(response.success){
+                            var data=response.data;
+                            var obj ={disco:url,data:data,target:target.parentElement.id};
+                            localStorage.setItem(url, JSON.stringify(obj));
+                             History.pushState(obj, data.title, '?disco='+location);
+                            
+                          }
+                        });
+          }
+        }
+           function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+          function playAudio() {
+            var music = document.getElementById('music');
+            if (music.paused) {
+              music.play();
+              pButton.className = "";
+              pButton.className = "pause";
+            } else { 
+              music.pause();
+              pButton.className = "";
+              pButton.className = "play";
+            }
+          }
+          function resetPlayAudio(){
+            pButton.className = "";
+              pButton.className = "play";
+
+          }
+          function stopAudio() {
+            if (!music.paused) {
+              music.pause();
+              pButton.className = "";
+              pButton.className = "play";
+            }
+          }
+      },
+      finalize: function() {
+        // JavaScript to be fired on all pages, after page specific JS is fired
       }
     }
+   
   };
 
   // The routing fires all common scripts, followed by the page specific scripts.
