83753f0a2cf8f038eb4fa4fb5695175ff7c7b7c9
Kilbourne <luca.castellone@gmail.com>
2016-06-06 18:04:46 +0200

    qqq

--- web/app/themes/osmedicine/base.php
+++ web/app/themes/osmedicine/base.php
@@ -52,6 +52,7 @@ use Roots\Sage\Wrapper;
       do_action('get_footer');
       get_template_part('templates/footer');?>
       <script>
+      
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-75826071-1', 'auto');
 ga('set', 'anonymizeIp', true);
