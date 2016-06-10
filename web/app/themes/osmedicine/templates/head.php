70958545a533e93395b0685837ec980572effba9
Kilbourne <luca.castellone@gmail.com>
2016-06-10 09:55:07 +0200

    change home

--- web/app/themes/osmedicine/templates/head.php
+++ web/app/themes/osmedicine/templates/head.php
@@ -4,10 +4,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php 
    $all_page_background_id=get_field('other_pages_background_image','option');
+   $home_page_background_id=get_field('full-width_background_image','option');   
    ?>
   <style>
   	.page-wrapper {    
       background-image: url(<?php echo wp_get_attachment_url( $all_page_background_id ); ?> );
+    }
+    body.home .page-wrapper {    
+      background-image: url(<?php echo wp_get_attachment_url( $home_page_background_id  ); ?> );
     }
   </style>
   <?php wp_head(); ?>
